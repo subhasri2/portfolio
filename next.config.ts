0fec2c5f6bcd028e56d7c4effeebeae743c89b88
subhasri2 <subhasri15214@gmail.com>
2025-11-27 14:00:12 -0600

    Initial project upload

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
   basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
   images: {
     unoptimized: true,
   },
